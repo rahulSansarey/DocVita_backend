8ecda32ed7b4dfdd22c5561da4fff8d2f047ee18
rahulSansarey <109734462+rahulSansarey@users.noreply.github.com>
2024-06-07 08:56:38 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -9,7 +9,7 @@ export const generateToken = (user, message, statusCode, res) => {
       ),
       httpOnly: true,
       secure: true,
-      sameSite: None,
+      sameSite: "None",
     })
     .json({
       success: true,
